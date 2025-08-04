0938395f0244fd21b211c0d3c76bebcb55c49af9
Kiaran Lau <kiaranlau@MacBook-Pro-10.local>
2025-08-04 06:53:23 -0700

    blank edit

--- src/readWebcam.py
+++ src/readWebcam.py
@@ -2,25 +2,24 @@ import numpy as np
 import cv2 as cv
 
 # img = cv.imread("src/static/p5r-joker-allout.webp")
-
 # cv.imshow("p5r allout attack", img)
-
 # cv.waitKey(0)
 
 
 def drawBlank():
     blank = np.zeros((500, 500, 3), dtype="uint8")
-    cv.rectangle(blank, (150, 150), (350, 350), (0, 155, 255), 2)
+    cv.rectangle(blank, (150, 150), (350, 350), (255, 155, 0), 2)
 
     cv.imshow("Blank", blank)
 
 
-def rescaleFrame(frame, scale=0.75):
+def rescaleFrame(frame, scale=0.5):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
     dim = (width, height)
 
+    frame = cv.flip(frame, 1)
     return cv.resize(frame, dim, interpolation=cv.INTER_AREA)
 
 
@@ -28,6 +27,7 @@ drawBlank()
 
 capture = cv.VideoCapture(0)
 
+# main
 while True:
     isTrue, frame = capture.read()
 
@@ -36,7 +36,7 @@ while True:
     # cv.imshow("MacOS Webcam", frame)
     cv.imshow("Macos Webcam resized", resized_frame)
 
-    if cv.waitKey(20) & 0xFF == ord("d"):  # bitwise operation to only take first bits
+    if cv.waitKey(20) & 0xFF == ord("q"):  # bitwise operation to only take first bits
         break
 
 capture.release()
