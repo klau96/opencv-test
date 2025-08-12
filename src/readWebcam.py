a4a3a3248541a6d96d0d225e526ad3e0076a7221
Kiaran Lau <kiaranlau@MacBook-Pro-10.local>
2025-08-12 16:39:42 -0700

    added edge cascade

--- src/readWebcam.py
+++ src/readWebcam.py
@@ -6,13 +6,6 @@ import cv2 as cv
 # cv.waitKey(0)
 
 
-def drawBlank():
-    blank = np.zeros((500, 500, 3), dtype="uint8")
-    cv.rectangle(blank, (150, 150), (350, 350), (255, 155, 0), 2)
-
-    cv.imshow("Blank", blank)
-
-
 def rescaleFrame(frame, scale=0.5):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
@@ -21,6 +14,26 @@ def rescaleFrame(frame, scale=0.5):
 
     frame = cv.flip(frame, 1)
     return cv.resize(frame, dim, interpolation=cv.INTER_AREA)
+
+
+# ==== Helper draw functions ====
+
+
+def drawBlank():
+    blank = np.zeros((500, 500, 3), dtype="uint8")
+    cv.rectangle(blank, (150, 150), (350, 350), (255, 155, 0), 2)
+
+    cv.imshow("Blank", blank)
+
+
+def drawGrayscale(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray", gray)
+
+
+def drawCannyEdges(img):
+    canny = cv.Canny(img, 125, 175)
+    cv.imshow("Canny Edges", canny)
 
 
 drawBlank()
@@ -34,7 +47,8 @@ while True:
     resized_frame = rescaleFrame(frame=frame)
 
     # cv.imshow("MacOS Webcam", frame)
-    cv.imshow("Macos Webcam resized", resized_frame)
+    # cv.imshow("Macos Webcam resized", resized_frame)
+    drawCannyEdges(resized_frame)
 
     if cv.waitKey(20) & 0xFF == ord("q"):  # bitwise operation to only take first bits
         break
