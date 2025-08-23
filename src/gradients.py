ce403fa29b735a8768dbc1098377446598e1fb13
Kiaran Lau <kiaranlau@MacBook-Pro-10.local>
2025-08-23 00:51:32 -0700

    edge detection template

---
+++ src/gradients.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("src/static/p5r-logo.png")
+cv.imshow("first image", img)
+
+
+# apply gaussian blur to remove noise
+def gaussianBlur(img, sigmas=(7, 7), border=0):
+    gauss = cv.GaussianBlur(img, sigmas, border)
+    cv.imshow("gaussian blur", gauss)
+    return gauss
+
+
+# apply laplacian operator to find gradients
+def laplacian(gray):
+    lap = cv.Laplacian(gray, cv.CV_64F)
+    cv.imshow("laplacian", lap)
+    return lap
+
+
+while True:
+    laplacian(img)
+    if cv.waitKey(20) & 0xFF == ord("q"):
+        break
+
+cv.destroyAllWindows()
