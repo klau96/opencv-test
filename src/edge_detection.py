dc3783dcf6083dc4329419d951d687da38034845
Kiaran Lau <kiaranlau@MacBook-Pro-10.local>
2025-08-24 17:07:16 -0700

    edge dtection added previous helper functions

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -5,7 +5,22 @@ img = cv.imread("src/static/p5r-logo.png")
 
 cv.imshow("img", img)
 
+
+# apply gaussian blur to remove noise
+def gaussianBlur(img, sigmas=(7, 7), border=0):
+    gauss = cv.GaussianBlur(img, sigmas, border)
+    cv.imshow("gaussian blur", gauss)
+    return gauss
+
+
 # laplacian
+# apply laplacian operator to find gradients
+def laplacian(gray):
+    lap = cv.Laplacian(gray, cv.CV_64F)
+    cv.imshow("laplacian", lap)
+    return lap
+
+
 # sobel x
 # sobel y
 # combined sobel
