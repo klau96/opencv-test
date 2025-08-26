0885a1b73e3b02bd0ae8e948de2770c441e36c17
Kiaran Lau <kiaranlau@MacBook-Pro-55.local>
2025-08-25 17:35:45 -0700

    face detection

---
+++ src/face_detect.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+img_path = os.path.join(dir, ".", "static", "mark_interview.jpg")
+img = cv.imread(img_path)
+
+
+# gray
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+
+# haar cascade
+haar_path = os.path.join(dir, "haarcascade.xml")
+haar_cascade = cv.CascadeClassifier(haar_path)
+
+faces_coord = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+print(f"face is at coord: {faces_coord}")
+
+for x, y, w, h in faces_coord:
+    cv.rectangle(img, (x, y), (x + w, y + h), (50, 255, 50), thickness=2)
+
+
+cv.imshow("Gray face", gray)
+cv.imshow("mark face detect", img)
+
+
+def whileLoop():
+    while True:
+        if cv.waitKey(20) & 0xFF == ord("q"):
+            break
+
+
+whileLoop()
+
+cv.destroyAllWindows()
